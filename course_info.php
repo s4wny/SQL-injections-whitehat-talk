45703dfcb29644838cfd619782da1418f46f2b3b
Andreas Wallström <awallst@kth.se>
2019-04-15 23:21:17 -0700

    view course info

--- course_info.php
+++ course_info.php
@@ -7,6 +7,27 @@ if(!isset($_SESSION["signed_in"]) || empty($_SESSION["signed_in"])) {
 	exit();
 }
 
+
+try {
+	$dbh = new PDO("mysql:host=localhost;charset=utf8;dbname=". DB_DB, DB_USER, DB_PASS);
+	$sth = $dbh->prepare("SELECT id, name FROM courses");
+	$sth->execute();
+	$courses = $sth->fetchAll();
+
+	if(isset($_GET["course_id"])) {
+		$sth = $dbh->prepare(
+			"SELECT * FROM courses
+			WHERE id = '". $_GET["course_id"] ."'
+		");
+		$sth->execute();
+		$specificCourse = $sth->fetchAll(PDO::FETCH_ASSOC);
+		$specificCourseCoulmns = array_keys($specificCourse[0]);
+	}
+
+} catch (PDOException $e) {
+    exit("DB Error!: " . $e->getMessage() . "<br/>");
+}
+
 include "view/header.php";
 include "view/course_info.php";
 include "view/footer.php";
--- view/course_info.php
+++ view/course_info.php
@@ -1,32 +1,35 @@
-<h2>Course info</h2>
+<h1>Course info</h1>
 <p>Select a course you want to know more about.</p>
 <form action="" method="get">
 	<select name="course_id" id="">
-		<option value="1">CSC</option>
-		<option value="2">asfd</option>
-		<option value="3">sfd</option>
-		<option value="4">safd</option>
+		<?php foreach($courses as $course): ?>
+			<option value="<?= $course['id'] ?>"><?= $course['name'] ?></option>
+		<?php endforeach; ?>
 	</select>
 	<input type="submit" value="Show">
 </form>
 
 <h2>Course</h2>
 
-<table>
-  <thead>
-    <tr>
-      <th>Make</th>
-      <th>Price</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>BMW X6</td>
-      <td>$75,000</td>
-    </tr>
-    <tr>
-      <td>Mercedes-Benz E350</td>
-      <td>$52,700</td>
-    </tr>
-  </tbody>
-</table>
+<?php if(isset($specificCourse)): ?> 
+	<table>
+	  <thead>
+	    <tr>
+	    	<?php foreach($specificCourseCoulmns as $column): ?>
+	     		<th><?= $column ?></th>
+  			<?php endforeach; ?>
+	    </tr>
+	  </thead>
+	  <tbody>
+    	<?php foreach($specificCourse as $row): ?>
+		    <tr>
+		      	<?php foreach($specificCourseCoulmns as $column): ?>
+	     			<th><?= $row[$column] ?></th>
+  				<?php endforeach; ?>
+		    </tr>
+		<?php endforeach; ?>
+	  </tbody>
+	</table>
+<?php else: ?>
+	<p>Select a course that you want to know more about</p>
+<?php endif; ?>
